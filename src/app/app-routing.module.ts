f7c86b6bd2a350d1182a03af97e7656ae5d9cbe4
Sinchana-RK <sinchanark9272@gmail.com>
2021-03-10 19:05:50 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AddTaskComponent } from './task/add-task/add-task.component';
+import { TaskDetailsComponent } from './task/task-details/task-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login',component:LoginComponent },
+  {path: 'signin',component:SigninComponent},
+  {path: 'signup',component:SignupComponent},
+  {path: 'taskDetails',component:AddTaskComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LoginComponent,SigninComponent,SignupComponent,AddTaskComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponentComponent } from './auth-component/auth-component.component';
-import { LoginComponentComponent } from './auth_module/login-component/login-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponentComponent,
-    LoginComponentComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
