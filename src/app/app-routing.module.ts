14b45b16a4a112c0e228be01c52b0b7ab304842a
Sinchana-RK <sinchanark9272@gmail.com>
2021-03-11 13:10:37 +0530

    included lazy loading modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { AddTaskComponent } from './task/add-task/add-task.component';
 import { TaskDetailsComponent } from './task/task-details/task-details.component';
 
 const routes: Routes = [
+  {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)},
+  {path:'task',loadChildren:()=>import('./task/task.module').then(m=>m.TaskModule)},
   {path : 'login',component:LoginComponent },
   {path: 'signin',component:SigninComponent},
   {path: 'signup',component:SignupComponent},
