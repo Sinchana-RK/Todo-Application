abb4fedee2a124cb75e7b66013a1d1b7d2d44f66
austinlewis16 <austin@campus.build>
2021-04-16 11:38:17 +0530

    includes changes on auth-module & it's inner components common-background implementation and minor changes to login-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, // to make sure every user gets login screen first
   { path: 'task', loadChildren: () => import('./task/task.module').then(m => m.TaskModule) }
-
 ];
 
 @NgModule({
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthcComponent } from './authc/authc.component';
 
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes:
-Routes=[{path:'', component: LoginComponent},
-{path:'signin', component: SigninComponent},
-{path:'signup', component: SignupComponent}
+Routes=[
+    {
+        path:'', component: AuthcComponent, // common/landing component for all authentication related works, it provides the essential background
+        children: [
+            { path: '', component: LoginComponent },
+            {path:'signin', component: SigninComponent},
+            {path:'signup', component: SignupComponent}
+        ]
+    },
 ];
 
 
